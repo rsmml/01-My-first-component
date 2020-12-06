60f672f249bdcacf6e039191e2ea94e7b617d4b4
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-06 17:05:07 +0100

    Article created

--- lib/01_article.jsx
+++ lib/01_article.jsx
@@ -1,9 +1,16 @@
 // TODO: import the right dependencies
-
-// TODO: code the Article component
-
+import React from 'react';
 
 
-
+const Article = () => {
+  return (
+    <div className="article">
+      <h2>My fancy new product</h2>
+      <p>Test it now!</p>
+    </div>
+  );
+};
 
 // TODO: export your Article component for tests
+
+export default Article;
